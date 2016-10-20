88185e49997e78c7e58873d5bc2d8243bd4b8650
Elmar Sonnenschein <eso@esoco.de>
2016-10-20 14:41:05 +0200

    FRAMEWORK-211: Deploy framework projects to JCenter and adapt build files accordingly
    Javadoc errors fixed

--- src/main/java/de/esoco/lib/net/oauth/GoogleApi20.java
+++ src/main/java/de/esoco/lib/net/oauth/GoogleApi20.java
@@ -1,12 +1,12 @@
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // This file is a part of the 'esoco-oauth' project.
-// Copyright 2015 Elmar Sonnenschein, esoco GmbH, Flensburg, Germany
+// Copyright 2016 Elmar Sonnenschein, esoco GmbH, Flensburg, Germany
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 3.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//	  http://www.apache.org/licenses/LICENSE-2.0
+//	  http://www.apache.org/licenses/LICENSE-3.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -42,7 +42,7 @@ import org.scribe.utils.Preconditions;
  * refresh token in it's 'secret' field.</p>
  *
  * @author yincrash
- * @see    https://gist.github.com/yincrash/2465453
+ * @see    "https://gist.github.com/yincrash/2465453"
  */
 public class GoogleApi20 extends DefaultApi20
 {
